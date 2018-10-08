b728ba11f9bc53dafb9aa355e48b2f57c0488d50
ihollander <ianholla@gmail.com>
2018-10-08 19:30:45 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
+def sort_array_asc(array)
+  array.sort{|a,b|
+    a <=> b
+  }
+end
 
+def sort_array_desc(array)
+  array.sort{|a,b|
+    b <=> a
+  }
+end
+
+def sort_array_char_count(array)
+  array.sort{|a,b|
+    a.length <=> b.length
+  }
+end
+
+def swap_elements_from_to(array,index,destination)
+  swap = array[index]
+  array[index] = array[destination]
+  array[destination] = swap
+  array
+end
+
+def swap_elements(array)
+  swap_elements_from_to(array,1,2)
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  new_array = []
+  array.each{|string|
+    string[2] = "$"
+    new_array << string
+  }
+  new_array
+end
+
+def find_a(array)
+  array.select{|string|
+    string.start_with?("a")
+  }
+end
+
+def sum_array(array)
+  array.reduce(:+)
+end
+
+def add_s(array)
+  array.each_with_index.collect{|item,index|
+    index != 1 ? "#{item}s" : item
+  }
+end
