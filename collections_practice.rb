3041b6155dc2e177e45f77db6293852f888739c5
magic-onion <alexander.m.truong@gmail.com>
2018-10-08 21:00:22 -0400

    done

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,18 +1,19 @@
 def sort_array_asc(array)
-
+array.sort
 end
 
 def sort_array_desc(array)
-
+array.sort {|x, y| y <=> x}
 end
 
 
 def sort_array_char_count(array)
-
+array.sort {|x, y| x.length <=> y.length}
 end
 
 def swap_elements(array)
-
+  array[1], array[2] = array[2], array[1]
+  array
 end
 
 def reverse_array(array)
