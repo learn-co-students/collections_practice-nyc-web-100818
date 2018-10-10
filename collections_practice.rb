867a23cd6bcda8eaf50c3dc7757404757e6493c6
github username <github email address>
2018-10-09 20:20:42 -0400

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -23,5 +23,36 @@ end
 
 # collections practice #reverse_array reverse the order of an array of integers
 def reverse_array(array)
-  array.reverse[array]
+array.reverse
+end
+
+# Collections practice #kesha_maker taking an array as an input, change the 3rd character of each element to a dollar sign.
+def kesha_maker(array)
+  new_array = []
+    array.each do |key|
+      new_array = key[2] = "$"
+    end
+end
+
+# Collections practice #find_a find all words that begin with "a" in the following array
+def  find_a(array)
+  if words_has_a = array.select {|words| words.start_with? ("a")}
+     words_has_a
+  end
+end
+
+# Collections practice #sum_array sum all the numbers in the following array
+def sum_array(integers)
+  integers.inject(0) { |result, element| result + element }
+end
+
+# Collections practice #add_s Add an "s" to each word in the array except for the 2nd element in the array
+def add_s(words)
+  words.each_with_index.collect do |element, index|
+  if index != 1
+    element += "s"
+  else
+    element = element
+    end
+  end
 end
